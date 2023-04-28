64cde2ef6994fea999f0cd41419239ee2f9747da
korrio <iamnotkorr@gmail.com>
2023-04-28 07:51:42 +0700

    profile

--- src/modules/singleUser/components/index.js
+++ src/modules/singleUser/components/index.js
@@ -18,6 +18,10 @@ import ListPostUserComponent from '@/modules/singleUser/components/listPostUser'
 import timeFormat from '@/common/utils/timeFormat';
 import style from '@/modules/singleUser/styles/style.module.scss';
 
+import TabHorizontalComponent from '@/common/components/TabHorizontal/components';
+
+import ProgressBar from 'react-bootstrap/ProgressBar';
+
 
 const SingleUserComponent = ({ singleUser, listPostUser }) => {
 	const { user } = useUser();
@@ -37,7 +41,7 @@ const SingleUserComponent = ({ singleUser, listPostUser }) => {
 								/>
 							</span>
 							{singleUser.data?.user_name !== user?.user_name && (
-								<div className={`d-flex position-absolute ${style.more__user}`}>
+								<div className={`d-flex d-none position-absolute ${style.more__user}`}>
 									<OverlayTrigger
 										trigger="click"
 										key="options-single-user"
@@ -67,6 +71,10 @@ const SingleUserComponent = ({ singleUser, listPostUser }) => {
 						<h4 className="text-break mb-1">
 							@{singleUser.data?.user_name}
 						</h4>
+						<h6 className="text-break mb-1">
+							{singleUser.data?.first_name} {singleUser.data?.last_name}
+						</h6>
+
 
 						{singleUser.data?.biography && <p className="text-break mb-2">{singleUser.data?.biography}</p>}
 						{user && singleUser.data?.user_name === user?.user_name && (
@@ -86,10 +94,47 @@ const SingleUserComponent = ({ singleUser, listPostUser }) => {
 							{singleUser.data?.total_user_followers} <span className="text-secondary">followers</span> ·{' '}
 							{singleUser.data?.total_following_users} <span className="text-secondary">following</span>
 						</div>
+						<div className="mt-1">
+							ระดับชั้นปัจจุบัน มัธยมศึกษาปีที่ 1
+							<ProgressBar>
+					      <ProgressBar striped variant="success" now={35} key={1} animated label="ม.1" />
+					     {/* <ProgressBar variant="warning" now={20} key={2} animated label="ม.2" />
+					      <ProgressBar striped variant="danger" now={10} animated key={3} label="ม.3" />*/}
+					    </ProgressBar>
+						</div>
 					</div>
 				</div>
+				<div className="col-lg-12 col-md-12 mb-10 mb-md-0">
+					<TabHorizontalComponent
+
+									items={[
+										{
+											title: 'โปรไฟล์',
+											slug: 'a1',
+											href: `#a1`
+										},
+										{
+											title: 'ความสำเร็จ',
+											slug: 'a3',
+											href: `#a3`
+										},
+										{
+											title: 'ผลงาน',
+											slug: 'a2',
+											href: `#a2`
+										},
+										
+									]}
+								/>
+				</div>
 				<div className="col-lg-4 col-md-4 mb-4 mb-md-0">
-					<h4>สถิติ</h4>
+								{/*<div class="tab-content" id="myTabContent">
+  <div class="tab-pane fade show active" id="a1" role="tabpanel" aria-labelledby="home-tab">1</div>
+  <div class="tab-pane fade" id="a2" role="tabpanel" aria-labelledby="profile-tab">2</div>
+  <div class="tab-pane fade" id="a3" role="tabpanel" aria-labelledby="contact-tab">3</div>
+</div>*/}
+
+					{/*<h4>สถิติ</h4>*/}
 					<ul className="wapper__card list-group rounded-16 shadow-sm">
 						<li className="list-group-item d-flex flex-wrap align-items-center">
 							<GrArticle className="me-1" />
@@ -117,32 +162,42 @@ const SingleUserComponent = ({ singleUser, listPostUser }) => {
 						<li className="list-group-item d-flex flex-wrap align-items-center">
 							<CustomImage
 								src={`/images/nft-rank-01.png`}
-								className="rounded-circle h-100 w-100"
-								width={33}
-								height={33}
+								className="rounded-circle h-100 w-100 badge-profile"
+								width={64}
+								height={64}
 								layout="fixed"
 							/>
-							<span>ผู้คงแก่เรียน</span>
+							<span>เข้าเรียนอย่างต่อเนื่อง 1 สัปดาห์</span>
+							<div className="col-lg-12">
+							<ProgressBar striped variant="success" now={100} key={1} animated label="1/10" />
+							</div>
 						</li>
 						<li className="list-group-item d-flex flex-wrap align-items-center">
 							<CustomImage
 								src={`/images/nft-rank-02.png`}
-								className="rounded-circle h-100 w-100"
-								width={33}
-								height={33}
+								className="rounded-circle h-100 w-100 badge-profile"
+								width={64}
+								height={64}
 								layout="fixed"
 							/>
-							<span>ผู้ไม่พลาดเป้า</span>
+							<span>ผู้เริ่มต้นลงมือทำ</span>
+							<div className="col-lg-12">
+								<ProgressBar striped variant="success" now={100} key={1} animated label="1/10" />
+							</div>
 						</li>
 						<li className="list-group-item d-flex flex-wrap align-items-center">
+						
 							<CustomImage
 								src={`/images/nft-rank-03.png`}
-								className="rounded-circle h-100 w-100"
-								width={33}
-								height={33}
+								className="rounded-circle h-100 w-100 badge-profile"
+								width={64}
+								height={64}
 								layout="fixed"
 							/>
-							<span>นักรบประจำสัปดาห์</span>
+							<span>ผู้บรรลุเป้าหมายขั้นต้น (ชิม)</span>
+							<div className="col-lg-12">
+								<ProgressBar striped variant="success" now={100} key={1} animated label="1/10" />
+							</div>
 						</li>
 
 					</ul>
